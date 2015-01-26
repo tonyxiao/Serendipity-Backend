38f5b8c7e2134fbfa582b7cd6391b99db5c1ec03
qimingfang <qf26@cornell.du>
2015-01-26 00:41:39 -0800

    initial commit. login with facebook and then fetch photos

---
+++ client/client.js
@@ -0,0 +1,20 @@
+Template.body.helpers({
+	photos: function() {
+		return Session.get("photos") || "";
+	}
+});
+
+Template.body.events({
+	'click button': function(event) {
+			Meteor.loginWithFacebook({
+				requestPermissions: ['email', 'user_photos']}, function (error) {
+			    if (error) {
+			        return console.log(error);
+			    }
+
+			    Meteor.call("getPicturesFromFacebook", function(err, result) {
+			    	Session.set('photos', result);
+			    });
+			});
+	}
+})
