a822ced803961ba461bc91a282b1850e196e67b5
Qiming Fang <qf26@cornell.edu>
2015-02-01 20:53:12 -0800

    need to expose the userData method via publish, like how it is described on meteor docs: http://docs.meteor.com/#/full/meteor_users

--- server/publish.js
+++ server/publish.js
@@ -1,3 +1,11 @@
+Meteor.publish("userData", function() {
+  if (this.userId) {
+    return Meteor.users.find({_id: this.userId});
+  } else {
+    this.ready();
+  }
+})
+
 Meteor.methods({
   nextMatch: function() {
     updateNextMatch(Meteor.user());
