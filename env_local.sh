ccef0088ced3e45757f22d8627e10e94cfcf3b0d
Qiming Fang <fang@ketch.co>
2015-02-26 21:58:16 -0800

    Ignoring env_local.sh because it contains sensative info

--- env_local.sh
+++
@@ -1,4 +0,0 @@
-export ROOT_URL="http://localhost:3000/"
-export FB_APPID="782247221868150"
-export AZURE_ACCOUNTID="s10"
-export AZURE_ACCESSKEY="Gcaevp8aPl3tEMV14caGOwKELWznSC4XIqZXzu1uV1rnck/kU/cUCn2AC02Y5ouOPPA/gBfybO2CNuzv1XD/KA=="
