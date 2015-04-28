7a4148f8250b9e0e89745c716d8c320508bc9d1c
Tony Xiao <tony@ketch.co>
2015-04-28 15:36:38 -0700

    Fix profile.d script to restore server

--- .profile.d/resolve_settings.sh
+++ .profile.d/resolve_settings.sh
@@ -2,10 +2,18 @@
 
 echo "-----> METEOR_SETTINGS resolved to -> $ENV"
 
+# TODO: Move logic into custom meteor buildpack
+# This type of script really belongs in the buildpack so that when we
+# scale the server it doesn't need to be launched with every new instance
+
+# Generate settings.json file
 GULP=node_modules/gulp/bin/gulp.js
 
 cd .scripts
 npm install --production
 $GULP settings
+
+# Go back to original directory, and set METEOR_SETTINGS
+cd ..
 export METEOR_SETTINGS=$(cat ../settings.json)
 
